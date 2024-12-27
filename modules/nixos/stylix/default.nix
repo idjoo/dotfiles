179db85baebd70a8abb55f683065784a6a7d6dc2
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2024-12-27 20:29:39 +0700

    feat: fonts to roboto

--- modules/nixos/stylix/default.nix
+++ modules/nixos/stylix/default.nix
@@ -51,8 +51,8 @@ in
         };
 
         monospace = {
-          package = pkgs.dank-mono-nerdfont;
-          name = "DankMono Nerd Font Mono";
+          package = (pkgs.nerdfonts.override { fonts = [ "RobotoMono" ]; });
+          name = "RobotoMono Nerd Font Mono";
         };
 
         emoji = {
