e23552ab26db8b1309ddbd9e7f021eb29b8993c1
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2024-12-31 15:44:47 +0700

    feat: add flameshot config

--- modules/home-manager/flameshot/default.nix
+++ modules/home-manager/flameshot/default.nix
@@ -12,8 +12,28 @@ in
     enable = mkEnableOption "flameshot";
   };
   config = mkIf cfg.enable {
+    systemd.user.targets.tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = [ "graphical-session-pre.target" ];
+      };
+    };
+
     services.flameshot = {
       enable = cfg.enable;
+
+      settings = {
+        General = {
+          savePath = "${config.home.homeDirectory}/pictures/screenshots";
+          savePathFixed = false;
+          uiColor = "#${config.lib.stylix.colors.base01}";
+          contrastUiColor = "#${config.lib.stylix.colors.base02}";
+          drawColor = "#${config.lib.stylix.colors.base03}";
+          disabledTrayIcon = true;
+          showStartupLaunchMessage = false;
+          saveAsFileExtension = ".png";
+        };
+      };
     };
   };
 }
