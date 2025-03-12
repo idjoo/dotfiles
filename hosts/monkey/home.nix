b9a40801f2c38c3fb46fa613e7fab480da57aa03
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2025-03-12 14:29:49 +0700

    wip

---
+++ hosts/monkey/home.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  outputs,
+  ...
+}:
+{
+  imports = [
+    outputs.homeManagerModules
+  ];
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    (google-cloud-sdk.withExtraComponents [
+      google-cloud-sdk.components.gke-gcloud-auth-plugin
+      google-cloud-sdk.components.log-streaming
+    ])
+  ];
+
+  modules = {
+    # shell
+    zsh.enable = true;
+
+    # editor
+    neovim.enable = true;
+
+    # cli
+    eza.enable = true;
+    git = {
+      enable = true;
+      email = "vian@idjo.cc";
+    };
+    gpg.enable = true;
+    lazygit.enable = true;
+    password-store.enable = true;
+    ssh.enable = true;
+    tmux.enable = true;
+    #fzf.enable = true;
+    direnv.enable = true;
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.05";
+}
