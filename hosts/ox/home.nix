e4516952735ddfb9bd57b3cae8f22eb2b4ea9a66
Adrianus Vian Habirowo <vian@idjo.cc>
2025-09-14 13:39:47 +0700

    feat(ox): change service as systemd unit

--- hosts/ox/home.nix
+++ hosts/ox/home.nix
@@ -3,6 +3,7 @@
 {
   pkgs,
   outputs,
+  config,
   ...
 }:
 {
@@ -41,8 +42,49 @@
     zsh.enable = true;
   };
 
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+  systemd.user = {
+    # Nicely reload system units when changing configs
+    startServices = "sd-switch";
+
+    services = {
+      whatsapp = {
+        Unit = {
+          Description = "Whatsapp Event Listener";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
+        Service = {
+          WorkingDirectory = "${config.home.homeDirectory}/documents/whatsapp";
+          ExecStart = "${config.home.homeDirectory}/documents/whatsapp/bin/whatsapp";
+        };
+      };
+      lumpang-be = {
+        Unit = {
+          Description = "SiLumpang Backend";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
+        Service = {
+          WorkingDirectory = "${config.home.homeDirectory}/documents/lumpang";
+          ExecStart = "${config.home.homeDirectory}/documents/lumpang/.venv/bin/uvicorn api:app --port=8001";
+        };
+      };
+      lumpang-fe = {
+        Unit = {
+          Description = "SiLumpang Frontend";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
+        Service = {
+          WorkingDirectory = "${config.home.homeDirectory}/documents/lumpang";
+          ExecStart = "${config.home.homeDirectory}/documents/lumpang/.venv/bin/streamlit run --browser.gatherUsageStats=false --server.port=8002 ui.py";
+        };
+      };
+    };
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
