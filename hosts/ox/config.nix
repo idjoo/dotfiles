ef6569722c15855d1b538d273ed77a5d1bfa5e3e
Adrianus Vian Habirowo <vian@idjo.cc>
2025-10-20 14:24:42 +0700

    feat(ox): enable docker virtualization

--- hosts/ox/config.nix
+++ hosts/ox/config.nix
@@ -79,6 +79,10 @@
     openssh.enable = true;
   };
 
+  virtualisation = {
+    docker.enable = true;
+  };
+
   # firewall
   networking.firewall = {
     enable = false;
@@ -180,15 +184,27 @@
       locations."/betting-dashboard/" = {
         proxyPass = "http://127.0.0.1:8005/betting-dashboard/";
       };
+
+      locations."~ /api/caribbe-commune/(.*)" = {
+        proxyPass = "http://127.0.0.1:8006/$1";
+      };
     };
   };
 
   services.postgresql = {
     enable = true;
-    ensureDatabases = [ "betting" ];
+    ensureDatabases = [
+      "betting"
+      "cc-cms"
+    ];
+    enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      # type  database  DBuser  origin-address  auth-method
+      local   all       all                     trust
+      # ipv4
+      host    all       all     127.0.0.1/32    trust
+      # ipv6
+      host    all       all     ::1/128         trust
     '';
   };
 
