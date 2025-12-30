f40af14ac299c082cdd9c79ec6487b394eb8e1ae
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2025-12-31 03:28:58 +0700

    feat(neovim): configure and enable avante with gemini-cli provider

--- modules/home-manager/neovim/plugins/avante.nix
+++ modules/home-manager/neovim/plugins/avante.nix
@@ -1,52 +1,30 @@
 {
+  pkgs,
+  config,
+  outputs,
+  ...
+}:
+{
   programs.nixvim.plugins.avante = {
     enable = true;
 
     settings = {
-      provider = "vertex";
+      provider = "gemini-cli";
 
-      providers = {
-        vertex = {
-          endpoint = "https://us-central1-aiplatform.googleapis.com/v1/projects/lv-playground-genai/locations/us-central1/publishers/google/models";
-          model = "gemini-2.5-flash";
-          extra_request_body = {
-            timeout = 30000;
-            temperature = 0;
-            max_tokens = 20480;
+      acp_providers = {
+        "gemini-cli" = {
+          command = "${pkgs.gemini-cli-bin}/bin/gemini";
+          args = [ "--experimental-acp" ];
+          env = {
+            NODE_NO_WARNINGS = "1";
+            CLI_TITLE = "${outputs.username}";
+            GOOGLE_APPLICATION_CREDENTIALS = "${config.home.homeDirectory}/.gemini/sa.json";
+            GOOGLE_CLOUD_PROJECT = "lv-playground-genai";
+            GOOGLE_CLOUD_LOCATION = "global";
           };
+          auth_method = "vertex-ai";
         };
       };
-
-      # system_prompt = {
-      #   __raw = ''
-      #     function()
-      #       local hub = require("mcphub").get_hub_instance()
-      #       return hub:get_active_servers_prompt()
-      #     end
-      #   '';
-      # };
-
-      custom_tools = {
-        __raw = ''
-          {
-            require("mcphub.extensions.avante").use_mcp_tool,
-            require("mcphub.extensions.avante").access_mcp_resource,
-          }
-        '';
-      };
-
-      disabled_tools = [
-        "list_files"
-        "search_files"
-        "read_file"
-        "create_file"
-        "rename_file"
-        "delete_file"
-        "create_dir"
-        "rename_dir"
-        "delete_dir"
-        "bash"
-      ];
     };
   };
 }
