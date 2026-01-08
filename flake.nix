c64f6b0d94416c6f0235abe6795192e34ce54e41
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2026-01-09 06:40:43 +0700

    feat(zen-browser): switch to beta channel and fix input follows
    Migrates to beta channel for latest features and ensures home-manager input follows the main flake input to avoid version conflicts.

--- flake.nix
+++ flake.nix
@@ -58,8 +58,9 @@
     };
 
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake/beta";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
