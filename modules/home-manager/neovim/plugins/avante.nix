5c3510e63e9b9f91aea4daa07d78601b99e18a36
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2026-03-06 03:10:04 +0700

    feat(neovim): ✨ switch avante provider to opencode

--- modules/home-manager/neovim/plugins/avante.nix
+++ modules/home-manager/neovim/plugins/avante.nix
@@ -13,20 +13,20 @@
     enable = true;
 
     settings = {
-      provider = "gemini-cli";
+      provider = "opencode";
 
       acp_providers = {
-        "gemini-cli" = {
-          command = "${pkgs.gemini-cli}/bin/gemini";
-          args = [ "--experimental-acp" ];
-          env = {
-            NODE_NO_WARNINGS = "1";
-            CLI_TITLE = "${outputs.lib.username}";
-            GOOGLE_APPLICATION_CREDENTIALS = config.sops.secrets."serviceAccounts/ai".path;
-            GOOGLE_CLOUD_PROJECT = "lv-playground-genai";
-            GOOGLE_CLOUD_LOCATION = "global";
-          };
-          auth_method = "vertex-ai";
+        "opencode" = {
+          command = "opencode";
+          args = [ "acp" ];
+          # env = {
+          #   NODE_NO_WARNINGS = "1";
+          #   CLI_TITLE = "${outputs.lib.username}";
+          #   GOOGLE_APPLICATION_CREDENTIALS = config.sops.secrets."serviceAccounts/ai".path;
+          #   GOOGLE_CLOUD_PROJECT = "lv-playground-genai";
+          #   GOOGLE_CLOUD_LOCATION = "global";
+          # };
+          # auth_method = "vertex-ai";
         };
       };
     };
